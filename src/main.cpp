ef28827639205d34b194f5d225eecd0d908635ff
Michael Krasselt <michael@michaelkrasselt.de>
2022-07-04 00:32:13 +0200

    Add: Wifi Config Interface

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,10 @@
   |   GND   |   GND   |   GND    |
   --------------------------------
  */
+#include <FS.h> //this needs to be first, or it all crashes and burns...
+#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+#include <SPIFFS.h>
+#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
 
 // This ESP_VS1053_Library
 #include <VS1053.h>
@@ -24,6 +28,7 @@
 #include "soc/rtc_cntl_reg.h"
 
 #define mqtt_base_topic "ESP32Mqtt-Streamer"
+#define mqtt_all_topics mqtt_base_topic "/#"
 #define mqtt_status_topic mqtt_base_topic "/status"
 #define mqtt_play_topic mqtt_base_topic "/play"
 #define mqtt_stream_topic mqtt_base_topic "/stream"
@@ -34,12 +39,12 @@
 #define mqtt_playtime_topic mqtt_base_topic "/playtime"
 
 // WiFi settings example, substitute your own
-const char *ssid = "SSID";
-const char *password = "PASSWD";
-
-const char *mqtt_server = "192.168.222.98";
-const char *mqtt_user = "openhabian";
-const char *mqtt_password = "openhabian";
+char mqtt_server[40];
+char mqtt_port[6] = "8080";
+char mqtt_user[40];
+char mqtt_passwd[40];
+char api_token[34] = "YOUR_API_TOKEN";
+bool shouldSaveConfig = false;
 
 const char *path = "http://streams.radiopsr.de/psr-live/mp3-64/mediaplayer";
 // const char *path = "http://mdr-284330-0.cast.mdr.de/mdr/284330/0/mp3/high/stream.mp3";
@@ -53,22 +58,23 @@ const char *path = "http://streams.radiopsr.de/psr-live/mp3-64/mediaplayer";
 WiFiClient WIFIClient;
 PubSubClient MQTTClient(WIFIClient);
 
+// callback notifying us of the need to save config
+void saveConfigCallback()
+{
+    Serial.println("Should save config");
+    shouldSaveConfig = true;
+}
+
 long lastReconnectAttempt = 0;
 
 boolean reconnect()
 {
-    if (MQTTClient.connect(mqtt_base_topic, mqtt_status_topic, 1, true, "offline"))
+    if (MQTTClient.connect(mqtt_base_topic, mqtt_user, mqtt_passwd, mqtt_status_topic, 1, true, "offline"))
     {
         // Once connected, publish an announcement...
         MQTTClient.publish(mqtt_status_topic, "online", true);
         // ... and resubscribe
-        MQTTClient.subscribe(mqtt_play_topic);
-        MQTTClient.subscribe(mqtt_play_topic);
-        MQTTClient.subscribe(mqtt_stop_topic);
-        MQTTClient.subscribe(mqtt_pauseplay_topic);
-        MQTTClient.subscribe(mqtt_stream_topic);
-        MQTTClient.subscribe(mqtt_file_topic);
-        MQTTClient.subscribe(mqtt_volume_topic);
+        MQTTClient.subscribe(mqtt_all_topics);
     }
     return MQTTClient.connected();
 }
@@ -78,7 +84,7 @@ void callback(char *topic, byte *payload, unsigned int length)
     char buffer[300];
     memcpy(buffer, payload, length);
     buffer[length] = '\0';
-    
+
     if (!strcmp(topic, mqtt_stream_topic))
     {
         playerTask::setStream(buffer);
@@ -107,7 +113,8 @@ void callback(char *topic, byte *payload, unsigned int length)
     {
         playerTask::setVolume(atoi(buffer));
     }
-    else{
+    else
+    {
         Serial.print("[Topic]");
         Serial.print(topic);
         Serial.print("[Payload]");
@@ -118,25 +125,143 @@ void callback(char *topic, byte *payload, unsigned int length)
 
 void setup()
 {
-    //disable brownout detection
-    //WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+    // disable brownout detection
+    // WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
     Serial.begin(115200);
-    Serial.print("Connecting to SSID ");
-    Serial.println(ssid);
+    Serial.println();
 
-    WiFi.begin(ssid, password);
+    // clean FS, for testing
+    // SPIFFS.format();
 
-    while (WiFi.status() != WL_CONNECTED)
+    // read configuration from FS json
+    Serial.println("mounting FS...");
+
+    if (SPIFFS.begin())
     {
-        delay(500);
-        Serial.print(".");
+        Serial.println("mounted file system");
+        if (SPIFFS.exists("/config.json"))
+        {
+            // file exists, reading and loading
+            Serial.println("reading config file");
+            File configFile = SPIFFS.open("/config.json", "r");
+            if (configFile)
+            {
+                Serial.println("opened config file");
+                size_t size = configFile.size();
+                // Allocate a buffer to store contents of the file.
+                std::unique_ptr<char[]> buf(new char[size]);
+
+                configFile.readBytes(buf.get(), size);
+
+                DynamicJsonDocument json(1024);
+                auto deserializeError = deserializeJson(json, buf.get());
+                serializeJson(json, Serial);
+                if (!deserializeError)
+                {
+
+                    Serial.println("\nparsed json");
+                    strcpy(mqtt_server, json["mqtt_server"]);
+                    strcpy(mqtt_user, json["mqtt_user"]);
+                    strcpy(mqtt_passwd, json["mqtt_passwd"]);
+                    strcpy(mqtt_port, json["mqtt_port"]);
+                    strcpy(api_token, json["api_token"]);
+                }
+                else
+                {
+                    Serial.println("failed to load json config");
+                }
+                configFile.close();
+            }
+        }
     }
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
+    else
+    {
+        Serial.println("failed to mount FS");
+    }
+    // end read
+
+    // The extra parameters to be configured (can be either global or just in the setup)
+    // After connecting, parameter.getValue() will get you the configured value
+    // id/name placeholder/prompt default length
+    WiFiManagerParameter custom_mqtt_server("server", "MQTT Server", mqtt_server, 40);
+    WiFiManagerParameter custom_mqtt_user("user", "MQTT User", mqtt_user, 40);
+    WiFiManagerParameter custom_mqtt_passwd("passwd", "MQTT Passwort", mqtt_passwd, 40);
+    WiFiManagerParameter custom_mqtt_port("port", "MQTT port", mqtt_port, 6);
+    WiFiManagerParameter custom_api_token("apikey", "API token", api_token, 32);
+
+    // WiFiManager
+    // Local initialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+
+    // set config save notify callback
+    wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+    // add all your parameters here
+    wifiManager.addParameter(&custom_mqtt_server);
+    wifiManager.addParameter(&custom_mqtt_user);
+    wifiManager.addParameter(&custom_mqtt_passwd);
+    wifiManager.addParameter(&custom_mqtt_port);
+    wifiManager.addParameter(&custom_api_token);
+
+    // reset settings - for testing
+    // wifiManager.resetSettings();
+
+    // if it does not connect it starts an access point with the specified name
+    // here  "AutoConnectAP"
+    // and goes into a blocking loop awaiting configuration
+    if (!wifiManager.autoConnect("ESP32MP3Streamer"))
+    {
+        Serial.println("failed to connect and hit timeout");
+        delay(3000);
+        // reset and try again, or maybe put it to deep sleep
+        ESP.restart();
+        delay(5000);
+    }
+
+    // if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+
+    // read updated parameters
+    strcpy(mqtt_server, custom_mqtt_server.getValue());
+    strcpy(mqtt_user, custom_mqtt_user.getValue());
+    strcpy(mqtt_passwd, custom_mqtt_passwd.getValue());
+    strcpy(mqtt_port, custom_mqtt_port.getValue());
+    strcpy(api_token, custom_api_token.getValue());
+    Serial.println("The values in the file are: ");
+    Serial.println("\tmqtt_server : " + String(mqtt_server));
+    Serial.println("\tmqtt_user : " + String(mqtt_user));
+    Serial.println("\tmqtt_passwd : " + String(mqtt_passwd));
+    Serial.println("\tmqtt_port : " + String(mqtt_port));
+    Serial.println("\tapi_token : " + String(api_token));
+
+    // save the custom parameters to FS
+    if (shouldSaveConfig)
+    {
+        Serial.println("saving config");
+        DynamicJsonDocument json(1024);
+        json["mqtt_server"] = mqtt_server;
+        json["mqtt_user"] = mqtt_user;
+        json["mqtt_passwd"] = mqtt_passwd;
+        json["mqtt_port"] = mqtt_port;
+        json["api_token"] = api_token;
+
+        File configFile = SPIFFS.open("/config.json", "w");
+        if (!configFile)
+        {
+            Serial.println("failed to open config file for writing");
+        }
+
+        serializeJson(json, Serial);
+        serializeJson(json, configFile);
+        configFile.close();
+        // end save
+    }
+
+    Serial.println("local ip");
     Serial.println(WiFi.localIP());
 
-    MQTTClient.setServer(mqtt_server, 1883);
     MQTTClient.setCallback(callback);
+    MQTTClient.setServer(mqtt_server, 1883);
 
     playerTask::init();
 }
